2baec1ad54ec830b995f99499ede863fefd0c599
Olubori-O <omogbejab@gmail.com>
2022-04-13 15:35:52 -0700

    1-alphabet.c task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
-
 /**
- * main - Entry point
- *
- * Return: nothing, return void
+ * print_alphabet - Entry point
+ * Return:0
  */
 
-int main(void)
+void print_alphabet(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	char c = 'a';
 
-return (0);
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
 }
